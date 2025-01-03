20b94706e5b214753e013d82cabc00b5aa201dfe
anet-eny <anet.eny@gmail.com>
2025-01-03 21:00:16 +0100

    Refactor Badge component, add props and classnames

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,25 @@ export default function App() {
             <div className="badges-container">
                 <div className="badges">
                     <h3>Square</h3>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
+                    <Badge variant="square" color="gray">Badge</Badge> 
+                    <Badge variant="square" color="red">Badge</Badge> 
+                    <Badge variant="square" color="yellow">Badge</Badge>  
+                    <Badge variant="square" color="green">Badge</Badge> 
+                    <Badge variant="square" color="blue">Badge</Badge> 
+                    <Badge variant="square" color="indigo">Badge</Badge>  
+                    <Badge variant="square" color="purple">Badge</Badge>  
+                    <Badge variant="square" color="pink">Badge</Badge> 
                 </div>
                 <div className="badges">
                     <h3>Pill</h3>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
-                    <Badge></Badge>
+                    <Badge variant="pill" color="gray">Badge</Badge> 
+                    <Badge variant="pill" color="red">Badge</Badge> 
+                    <Badge variant="pill" color="yellow">Badge</Badge>  
+                    <Badge variant="pill" color="green">Badge</Badge> 
+                    <Badge variant="pill" color="blue">Badge</Badge> 
+                    <Badge variant="pill" color="indigo">Badge</Badge>  
+                    <Badge variant="pill" color="purple">Badge</Badge>  
+                    <Badge variant="pill" color="pink">Badge</Badge>
                 </div>
             </div>
         </div>
--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import classNames from 'classnames'
 
-export default function Badge() {
+export default function Badge({children, variant, color}) {
+    const allClasses = classNames(color, variant, "badge")
+
     return (
         <div className="badge-container">
-            <p className="badge-label">Gray - Square</p>
-            <div className="badge">
-                Badge
+            <p className="badge-label">{`${color.charAt(0).toUpperCase() + color.slice(1)} - Square`}</p>
+            <div className={allClasses}>
+                {children}
             </div>
         </div>
     )
