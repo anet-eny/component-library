4ff884235e0726cd98c596e60a1bdb0c607addd9
anet-eny <anet.eny@gmail.com>
2025-01-03 16:27:28 +0100

    Create basic structure and style for badges section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import React from "react"
 
 export default function App() {
     return (
-        <h1>Test</h1>
+        <div>
+            <h1>Component Library</h1>
+            <h2>Badges</h2>
+            <div className="badges-container">
+                <div className="squares">
+                    <h3>Square</h3>
+                </div>
+                <div className="pills">
+                    <h3>Pill</h3>
+
+                </div>
+                
+            </div>
+        </div>
+        
     )
 }
