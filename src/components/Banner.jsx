3f48555dc3d0a0e83c2acc9bfd221a364da19171
anet-eny <anet.eny@gmail.com>
2025-01-04 22:03:16 +0100

    Import icons and set icon logic based on status prop

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,25 @@
 import React from "react"
 import classNames from 'classnames'
 import { FaCheckCircle } from "react-icons/fa"
+import { FaExclamationTriangle } from "react-icons/fa"
+import { FaCircleXmark } from "react-icons/fa6"
+import { FaInfoCircle } from "react-icons/fa"
+import { TbFileNeutral } from "react-icons/tb"
+
 
 export default function Banner(props) {
+    const allClasses = classNames(props.status, "banner")
+
+    const icon = 
+        props.status === "success" ? <FaCheckCircle /> : 
+        props.status === "warning" ? <FaExclamationTriangle /> : 
+        props.status === "error" ? <FaCircleXmark /> : 
+        props.status === "neutral" ? <FaInfoCircle /> : 
+        <TbFileNeutral />
+
     return (
-        <div className="banner">
-            <FaCheckCircle />
+        <div className={allClasses}>
+            {icon}
             <div className="banner-text">
                 <p><strong>{props.title}</strong></p>
                 <p>{props.text}</p>
