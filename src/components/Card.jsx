e62ed46d701d66d8e25d01703448b464001f2cf0
anet-eny <anet.eny@gmail.com>
2025-01-05 21:35:42 +0100

    Add labels for cards, fix card layout css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,17 @@ export default function Card(props) {
     const hoverClass = props.hover? "hover" : ""
 
     return (
-        <div className="card">
-            <div className="card-icon">
-                {props.icon}
+        <div>
+            <p>{`Card ${hoverClass}`}</p>
+            <div className="card">
+                <div className="card-icon">
+                    {props.icon}
+                </div>
+                <div className={`card-content ${hoverClass}`}>
+                    <h4>{props.title}</h4>
+                    <p>{props.text}</p>
+                </div>
             </div>
-            <div className={`card-content ${hoverClass}`}>
-                <h4>{props.title}</h4>
-                <p>{props.text}</p>
-             </div>
         </div>
     )
 }
